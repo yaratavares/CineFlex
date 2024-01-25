3c345b46dc71d0438d6c44a68262d496a4016676
Yara Tavares <yaracriistavares@gmail.com>
2024-01-25 09:54:30 -0300

    feat: add certificate to route 53 domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,9 @@ provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
 }
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = var.aws_profile
+  alias   = "us-east-1"
+}
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,5 @@
 aws_region  = "sa-east-1"
 aws_profile = "tf01"
 domain_name = "cineflex"
+domain      = "cineflex.click"
+environment = "prod"
